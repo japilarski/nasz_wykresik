a1a6b1ee38839011b5e355549cd02280a9a4b297
Mateusz Pałczyński <mateusz.palczynski1@gmail.com>
2024-11-20 20:02:26 +0100

    feat:plot

--- temp.R
+++ temp.R
@@ -1,3 +1,13 @@
 install.packages("tidyverse")
 
 library(tidyverse)
+data(mpg)
+ggplot(data = mpg, aes(x = displ, y = hwy, color = drv)) +
+  geom_point() +
+  labs(
+    title = "Zależność między pojemnością silnika a zużyciem paliwa",
+    x = "Pojemność silnika (displ)",
+    y = "Zużycie paliwa na autostradzie (hwy)",
+    color = "Napęd (drv)"
+  ) +
+  theme_minimal()
